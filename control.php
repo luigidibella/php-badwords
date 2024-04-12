3101d3cb1ef4f0fa0eabcaa1c28d13f643d67299
luigidibella <lykos.omega@gmail.com>
2024-04-12 17:11:39 +0200

    [add] all

---
+++ control.php
@@ -0,0 +1,46 @@
+<?php
+
+$text = trim($_POST['text']);
+$censored = trim($_POST['censored']);
+$lenghtText = strlen($text);
+$textCensored = str_replace($censored, '***', $text);
+$lenghtCensoredText = strlen($textCensored);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
+  <title>control</title>
+</head>
+
+<body>
+  <div class="container text-center my-3 bg-body-secondary p-3">
+
+    <h1>control</h1>
+
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" value="<?php echo $text ?>" readonly>
+      <span class="input-group-text">
+        <?php echo $lenghtText ?>
+      </span>
+    </div>
+
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" value="<?php echo $textCensored ?>" readonly>
+      <span class="input-group-text">
+        <?php echo $lenghtCensoredText ?>
+      </span>
+    </div>
+
+    <div class="btn btn-danger">
+      <a href="./" class="text-white text-decoration-none">Indietro</a>
+    </div>
+      
+  </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
+  <title>php-badwords</title>
+</head>
+<body>
+
+  <div class="container text-center my-3 bg-body-secondary p-3">
+
+    <h1>php-badwords</h1>
+
+
+    <form action="control.php" method="POST">
+      <input name="text" class="form-control mb-3" aria-label="With input"></input>
+
+      <input name="censored" class="form-control mb-3" aria-label="With input"></input>
+
+      <button class="btn btn-danger" type="submit">Invia</button>
+    </form>
+  </div>
+  
+</body>
+</html>
